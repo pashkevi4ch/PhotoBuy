46860c77338d34045cf70eb33bc74062774e3458
Anna Savina <sav.ahha321@gmail.com>
2020-10-09 13:33:12 +0300

    update DB

--- PhotoBuy/PhotoBuy/App.xaml.cs
+++ PhotoBuy/PhotoBuy/App.xaml.cs
@@ -2,11 +2,43 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using PhotoBuy.Pages;
+using System.IO;
+using PhotoBuy.DataBase;
+using System.Collections.Generic;
 
 namespace PhotoBuy
 {
     public partial class App : Application
     {
+        public const string DATABASE_NAME_PreviousRequests = "PreviousRequests.db";
+        static RequestDatabase databasePreviousRequests;
+        public static RequestDatabase DatabasePreviousRequests
+        {
+            get
+            {
+                if (databasePreviousRequests == null)
+                {
+                    databasePreviousRequests = new RequestDatabase(
+                        Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME_PreviousRequests));
+                }
+                return databasePreviousRequests;
+            }
+        }
+
+        public static IList<Request> PreviousRequests
+        {
+            get
+            {
+                List<Request> requests = new List<Request>();
+                if (databasePreviousRequests != null)
+                {
+                    fairProjects = App.DatabasePreviousRequests.GetRequestsAsync().Result;
+                }
+                return requests;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
