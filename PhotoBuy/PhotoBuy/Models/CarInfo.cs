308fd5d9525f6c58186f182f6679abe92aae0ad8
Pavel <petrov.pavel.v14@gmail.com>
2020-10-10 17:58:31 +0300

    fixed api 2

--- PhotoBuy/PhotoBuy/Models/CarInfo.cs
+++ PhotoBuy/PhotoBuy/Models/CarInfo.cs
@@ -12,9 +12,9 @@ namespace PhotoBuy.Models
         public string Model { get; set; }
         public string OwnTitle { get; set; }
         public int MinPrice { get; set; }
-        public List<string> RenderPhotos { get; set; }
+        public string RenderPhotos { get; set; }
         public int ColorsCount { get; set; }
-        public List<string> SizesPhotos { get; set; }
+        public string SizesPhotos { get; set; }
         public decimal Probability { get; set; }
 
     }
