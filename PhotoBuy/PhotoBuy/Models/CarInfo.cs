2fadf006f02f29609f30b2956729b91eb75a1987
Pavel <petrov.pavel.v14@gmail.com>
2020-10-10 16:38:39 +0300

    API 2.0

---
+++ PhotoBuy/PhotoBuy/Models/CarInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace PhotoBuy.Models
+{
+    public class CarInfo
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Logo { get; set; }
+        public string Model { get; set; }
+        public string OwnTitle { get; set; }
+        public int MinPrice { get; set; }
+        public List<string> RenderPhotos { get; set; }
+        public int ColorsCount { get; set; }
+        public List<string> SizesPhotos { get; set; }
+        public decimal Probability { get; set; }
+
+    }
+}
